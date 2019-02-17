c8ecbfca4469ab38fe2b8bc3ccaa3473e87da6dc
Jager <ge@zhangge.net>
2019-02-17 15:59:31 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# remove old version
+# remove old version only for centos
 yum remove docker \
     docker-client \
     docker-client-latest \
@@ -9,15 +9,15 @@ yum remove docker \
     docker-logrotate \
     docker-engine
 
-# add repo and install latest version
+# add repo and install latest version only for centos
 yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io
 
 # start and register to startup
 service docker start
 chkconfig docker on
-
-# install docker-compose
+# =========================================================
+# install docker-compose only foy linux
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
